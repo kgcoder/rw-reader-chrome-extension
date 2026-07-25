ffe21796c79a310a56e75d01c9a57909da1140ae
Karen Grigorian <kgcoder@users.noreply.github.com>
2026-07-25 16:06:17 +0400

    add guard

--- extension/reader/ReadingManager.js
+++ extension/reader/ReadingManager.js
@@ -2114,6 +2114,7 @@ setupFlinksCanvasDPR(){
     
             for(let flink of flinks){
                 if (flink.rightEndOutOfBounds) continue
+                if(!flink.rightRects)continue
                 for(let rect of flink.rightRects){
 
                     if(isDotInsideFrame(x,y,{minX:rect.left,minY:rect.top,maxX:rect.left + rect.width,maxY:rect.top + rect.height})){
